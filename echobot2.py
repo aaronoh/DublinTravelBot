e4a60acea48cbb55f76d9c0c96e93b86d7273c6b
Aaron O'Hare <aaronoh96@gmail.com>
2017-11-26 17:59:32 +0000

    Retrieves XML data from Irish Rail, converts it to a dict, parses that dict into a json string and coverts it to a json object. Navigates that object and outputs the minutes remaining for the next train to Malahide

---
+++ echobot2.py
@@ -0,0 +1,78 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, BaseFilter
+import logging
+import requests, xmltodict, json
+
+
+class FilterTrain(BaseFilter):
+    def filter(self, message):
+        return 'train' in message.text.lower()
+        #any(text.lower() in message.text.lower() for text in ('Foo', 'Bar'))
+
+# Remember to initialize the class.
+filter_train = FilterTrain()
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+
+def train(bot, update):
+    xml = requests.get('http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode=mhide')
+    dict = xmltodict.parse(xml.content)
+    jsonstr = json.dumps(dict)
+    jsonobj = json.loads(jsonstr)
+    update.message.reply_text(jsonobj["ArrayOfObjStationData"]["objStationData"])
+    nextTrainToMal = jsonobj["ArrayOfObjStationData"]["objStationData"][1]["Duein"]
+    update.message.reply_text("The next train to Malahide is due in " + nextTrainToMal + " minutes")
+
+
+def showme(bot, update):
+    update.message.reply_text('Your train is due in 10 mintues.')
+    update.message.reply_text('Click on the map below for directions.')
+    bot.sendLocation(166047440, latitude=53.328958, longitude=-6.219859);
+
+
+def thanks(bot, update):
+    update.message.reply_text("You're Welcome! Let me know if you need anything else.")
+
+
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    # Create the EventHandler and pass in bot's token.
+    updater = Updater("460295615:AAEUzHYLg9s1f6YNr1Ng2s5dKMv27lZZcWE")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(MessageHandler(filter_train, train))
+    dp.add_handler(CommandHandler("showme", showme))
+    dp.add_handler(CommandHandler("Thanks", thanks))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    
+
+
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
