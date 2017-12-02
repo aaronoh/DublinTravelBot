6db65d4182c208a6196d6aa1fc6e9f691c200184
Aaron O'Hare <aaronoh96@gmail.com>
2017-12-02 18:33:00 +0000

    Added ability to retrieve station availability by station name. Send location now works porperly - sends to correct chat id and pulls station from initial search

--- dtbot.py
+++ dtbot.py
@@ -1,8 +1,10 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, BaseFilter
 import logging
 import requests, xmltodict, json
+from pprint import pprint
 
 i = 0
+myStation = "";
 
 
 class FilterTrain(BaseFilter):
@@ -23,27 +25,61 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def train(bot, update):
-    xml = requests.get('http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode=SMONT')
+    #This works assuming the user inputs 'Train StationName'
+    msg = update.message.text.split()
+    if msg[1].lower() == 'dun' or msg[1].lower() == 'dublin' or msg[1].lower() == 'clontarf'  or msg[1].lower() == 'tara':
+        sname = msg[1] + " " + msg[2]
+
+    else:
+        sname = msg[1]
+
+
+    url = 'http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc={0}'.format(sname)
+    print(url)
+    xml = requests.get(url)
     dict = xmltodict.parse(xml.content)
     jsonstr = json.dumps(dict)
     jsonobj = json.loads(jsonstr)
+    dueIn = (jsonobj["ArrayOfObjStationData"]["objStationData"][0]["Duein"])
+    stationName = (jsonobj["ArrayOfObjStationData"]["objStationData"][0]["Stationfullname"])
+    destination = (jsonobj["ArrayOfObjStationData"]["objStationData"][0]["Destination"])
+    #update.message.reply_text(jsonobj["ArrayOfObjStationData"]["objStationData"][1])
+    update.message.reply_text("The next train to service the {0} station is heading for {1}, it's due in {2} minutes.".format(stationName, destination, dueIn))
 
-    dueIn = (jsonobj["ArrayOfObjStationData"]["objStationData"][1]["Duein"])
-    stationName = (jsonobj["ArrayOfObjStationData"]["objStationData"][1]["Stationfullname"])
-    destination = (jsonobj["ArrayOfObjStationData"]["objStationData"][1]["Destination"])
-    update.message.reply_text(jsonobj["ArrayOfObjStationData"]["objStationData"][1])
-    update.message.reply_text("The next train to service the " + stationName + " station is heading for " + destination + ", it's due in " + dueIn + " minutes.")
+    print(update.message.text)
 
     global i
     i += 1
     print("This has been called" , i , "times.")
 
+    global myStation
+    myStation= (jsonobj["ArrayOfObjStationData"]["objStationData"][1]["Stationfullname"])
+
 
 
 def showme(bot, update):
-    update.message.reply_text('Your train is due in 10 mintues.')
-    update.message.reply_text('Click on the map below for directions.')
-    bot.sendLocation(166047440, latitude=53.328958, longitude=-6.219859);
+    #getting lat/long
+    global myStation
+
+    url = 'http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML_WithStationType?StationType=D'
+    xml = requests.get(url)
+    dict = xmltodict.parse(xml.content)
+    jsonstr = json.dumps(dict)
+    jsonobj = json.loads(jsonstr)
+
+    x = jsonobj["ArrayOfObjStation"]["objStation"][0]
+
+    for attrs in jsonobj["ArrayOfObjStation"]["objStation"]:
+        if attrs['StationDesc'] == myStation:
+            print ( "This is the shit:  {0}".format(attrs))
+            lat = attrs['StationLatitude']
+            long = attrs['StationLongitude']
+            break
+
+    update.message.reply_text("See map below for directions to the {0} station".format(myStation, ))
+    #id = effective_chat
+
+    bot.sendLocation(update.effective_chat.id, latitude=lat, longitude=long);
 
 
 def thanks(bot, update):
@@ -59,7 +95,6 @@ def error(bot, update, error):
 
 
 def main():
-
     # Create the EventHandler and pass in bot's token.
     updater = Updater("460295615:AAEUzHYLg9s1f6YNr1Ng2s5dKMv27lZZcWE")
 
