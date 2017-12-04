c6a50d26f6ba320d0d3fe6c2d53078eb91dd32c0
Aaron O'Hare <aaronoh96@gmail.com>
2017-12-04 19:02:16 +0000

    Added ability to define direction when searching for a train Added list of commands to bot father, added ability to list all active stations

--- dtbot.py
+++ dtbot.py
@@ -1,18 +1,18 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, BaseFilter
-import logging
-import requests, xmltodict, json
-from pprint import pprint
+import logging,requests, xmltodict, json
 
+#Keep track of calls
 i = 0
+#Global var for station name - used by train and showme
 myStation = "";
 
-
+#Filter messages for 'train'
 class FilterTrain(BaseFilter):
     def filter(self, message):
         return 'train' in message.text.lower()
         #any(text.lower() in message.text.lower() for text in ('Foo', 'Bar'))
 
-# Remember to initialize the class.
+# Initialize filter class.
 filter_train = FilterTrain()
 
 # Enable logging
@@ -22,69 +22,108 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
+# Pull stations from api, parse into json, construct string of relevant info, return to user
 def train(bot, update):
-    #This works assuming the user inputs 'Train StationName'
+
+    #This works assuming the user inputs 'Train StationName Direction'
+    #Splitting message into elements - message assumes 'train - stationName - Direction' format, meaning [1] would be the station name & [2] the direction.
+    #If/else - really messy way of dealing with multiple word station names
     msg = update.message.text.split()
     if msg[1].lower() == 'dun' or msg[1].lower() == 'dublin' or msg[1].lower() == 'clontarf'  or msg[1].lower() == 'tara':
         sname = msg[1] + " " + msg[2]
+        direction = msg[3]
+
+    elif msg[1].lower() == 'grand':
+        sname = msg[1] + " " + msg[2] + " " + msg[3]
+        direction = msg[4]
 
     else:
         sname = msg[1]
+        direction =  msg[2]
 
+    #Attributes are case sensitive - account for that
+    if direction == "northbound":
+            direction = "Northbound"
+
+    if direction == "southbound":
+            direction = "Southbound"
 
     url = 'http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc={0}'.format(sname)
-    print(url)
+    #xml -> dict -> json str -> json obj
     xml = requests.get(url)
     dict = xmltodict.parse(xml.content)
     jsonstr = json.dumps(dict)
     jsonobj = json.loads(jsonstr)
-    dueIn = (jsonobj["ArrayOfObjStationData"]["objStationData"][0]["Duein"])
-    stationName = (jsonobj["ArrayOfObjStationData"]["objStationData"][0]["Stationfullname"])
-    destination = (jsonobj["ArrayOfObjStationData"]["objStationData"][0]["Destination"])
-    #update.message.reply_text(jsonobj["ArrayOfObjStationData"]["objStationData"][1])
-    update.message.reply_text("The next train to service the {0} station is heading for {1}, it's due in {2} minutes.".format(stationName, destination, dueIn))
+
+    # For every object in the json obj
+    for attrs in jsonobj["ArrayOfObjStationData"]["objStationData"]:
+        #if the direction matches the requested direction
+        if attrs['Direction'] == direction:
+            # Pull out specific elements of the jsonobj
+            dueIn = (attrs["Duein"])
+            stationName = (attrs["Stationfullname"])
+            destination = (attrs["Destination"])
+            dir = (attrs["Direction"])
+            break
+
+    #Return worthwhile string to user
+    update.message.reply_text("The next {0} train to service the {1} station is heading for {2}, it's due in {3} minutes.".format(dir, stationName, destination, dueIn))
 
     print(update.message.text)
-
+    #Keeping track of the number of times train has been called
     global i
     i += 1
     print("This has been called" , i , "times.")
 
+    #Setting global var station name to the name of the station just searched for
     global myStation
     myStation= (jsonobj["ArrayOfObjStationData"]["objStationData"][1]["Stationfullname"])
 
 
 
 def showme(bot, update):
-    #getting lat/long
+    #retrieves global station name
     global myStation
 
+    #Poll api again - using the station info endpoint as this is the only way of getting their lat/long
+    # xml -> dict -> json str -> json obj
     url = 'http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML_WithStationType?StationType=D'
     xml = requests.get(url)
     dict = xmltodict.parse(xml.content)
     jsonstr = json.dumps(dict)
     jsonobj = json.loads(jsonstr)
 
-    x = jsonobj["ArrayOfObjStation"]["objStation"][0]
-
+    # For every object in the json obj
     for attrs in jsonobj["ArrayOfObjStation"]["objStation"]:
+        #if the station name attribute matches the global var station name
         if attrs['StationDesc'] == myStation:
-            print ( "This is the shit:  {0}".format(attrs))
+            #lat/long vars = the lat/long attributes associated with that object
             lat = attrs['StationLatitude']
             long = attrs['StationLongitude']
+            #Once its' matched one, break out of the for loop
             break
-
+    #Return a worhtwhile string to the user using the above information
     update.message.reply_text("See map below for directions to the {0} station".format(myStation, ))
-    #id = effective_chat
 
+    #Send a map to the user - retrieve the chat id from the original function call and use the lat/lng vars set above
     bot.sendLocation(update.effective_chat.id, latitude=lat, longitude=long);
 
-
-def thanks(bot, update):
-    update.message.reply_text("You're Welcome! Let me know if you need anything else.")
-
+def liststations(bot, update):
+    # Poll api again - using the station info endpoint
+    # xml -> dict -> json str -> json obj
+    url = 'http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML_WithStationType?StationType=D'
+    xml = requests.get(url)
+    dict = xmltodict.parse(xml.content)
+    jsonstr = json.dumps(dict)
+    jsonobj = json.loads(jsonstr)
+    list_of_stations = ""
+    # For every object in the json obj
+    for attrs in jsonobj["ArrayOfObjStation"]["objStation"]:
+        # add the stationdesc attribute to the string list_of_stations
+        list_of_stations += (attrs['StationDesc'] + ", ")
+    # Return a worhtwhile string to the user using the above information
+    update.message.reply_text("Here is a list of all stations currently operating DART services: {0}".format(list_of_stations))
+    update.message.reply_text("To view live availability information for any of these stops simply type 'Train', the name of the station and the direction. For example,  Train Sandymount northbound")
 
 def echo(bot, update):
     update.message.reply_text(update.message.text)
@@ -95,27 +134,27 @@ def error(bot, update, error):
 
 
 def main():
-    # Create the EventHandler and pass in bot's token.
+    # Create EventHandler, pass in api key.
     updater = Updater("460295615:AAEUzHYLg9s1f6YNr1Ng2s5dKMv27lZZcWE")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
+    # when a message triggers the filter_train, run the train function
     dp.add_handler(MessageHandler(filter_train, train))
+    # when a message contains a defined command (/showme, /thanks, run the thanks/showme function)
     dp.add_handler(CommandHandler("showme", showme))
-    dp.add_handler(CommandHandler("Thanks", thanks))
+    dp.add_handler(CommandHandler("list", liststations))
 
-    # on noncommand i.e message - echo the message on Telegram
+    # test handler - when a message that contains text is received - trigger the echo function
     dp.add_handler(MessageHandler(Filters.text, echo))
     
 
-
+    #add error handler
     dp.add_error_handler(error)
 
     # Start the Bot
     updater.start_polling()
-
     updater.idle()
 
 
